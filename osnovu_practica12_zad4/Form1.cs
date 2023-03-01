cecf1cc0b1331138fc89ee90304b1d5d1f31eb4b
VirtoriaECTS <123994483+VirtoriaECTS@users.noreply.github.com>
2023-03-01 19:48:38 +0500

    Add files via upload

---
+++ osnovu_practica12_zad4/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace osnovu_practica12_zad4
+{
+    public partial class Form1 : Form
+    {
+        public bool chek()
+        {
+            int n = 0;
+            if (textBox1.Text.Length > 0 && textBox2.Text.Length > 0 && textBox3.Text.Length > 0 && textBox4.Text.Length > 0) n += 1;
+            if (radioButton1.Checked || radioButton2.Checked) n += 1;
+            if(n == 2) return true;
+            else return false;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label9.Visible= false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label9.Visible = false;
+
+            if (chek())
+            {
+                //Присвоение переменных
+                Sportsmen human = new Sportsmen();
+
+                human.familia = textBox3.Text;
+                human.otchestvo = textBox2.Text;
+                human.name = textBox1.Text;
+
+                string day = Convert.ToString(numericUpDown3.Value);
+                string month = Convert.ToString(numericUpDown4.Value);
+                string year = Convert.ToString(numericUpDown5.Value);
+                human.data_roz = Convert.ToDateTime(day + "/" + month + "/" + year);
+
+                human.vid_sporta = textBox4.Text;
+
+                human.rost = Convert.ToDouble(numericUpDown1.Value);
+                human.ves = Convert.ToDouble(numericUpDown2.Value);
+
+                if (radioButton1.Checked)
+                {
+                    human.pol = "М";
+                }
+                else human.pol = "Ж";
+
+                //Выполнение фунций
+                label14.Text = human.get_info();
+                label15.Text = Convert.ToString(human.Brok_ves());
+                label16.Text = Convert.ToString(human.Kuper_ves());
+                label17.Text = human.imt();
+
+            }
+            else
+            {
+                label9.Visible = true;
+                label9.Text = "Необходимо заполнить все поля";
+                label14.Text = "";
+                label15.Text = "";
+                label16.Text = "";
+                label17.Text = "";
+            }
+
+           
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label14.Text = "";
+            label15.Text = "";
+            label16.Text = "";
+            label17.Text = "";
+            label9.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            radioButton1 .Checked = false;
+            radioButton2 .Checked = false;
+        }
+    }
+}
---
+++ osnovu_practica12_zad4/Sportsmen.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osnovu_practica12_zad4
+{
+    public class Sportsmen
+    {
+        public string name;
+        public string otchestvo;
+        public string familia;
+        public string pol;
+        public DateTime data_roz;
+        public double rost;
+        public double ves;
+        public string vid_sporta;
+
+        public string get_info() //Информация о спортсмене
+        {
+            string text = "Информация о спортсмене";
+            text += $"\nИмя: {name}\nОтчество: {otchestvo}\nФамилия: {familia}\nПол: {pol}\n" +
+                $"Дата рождения:{data_roz.ToString("dd MMMM yyyy")}\nРост:{rost}" +
+                $"\nВес: {ves}\nВид спорта:{vid_sporta}\n";
+            return text;
+        }
+
+        public double Brok_ves() //Идеальный вес по формуле брока
+        {
+            double perfuct_ves;
+
+            int year_age = Convert.ToInt32(data_roz.ToString("yyyy"));
+            int age = 2023 - year_age;
+            if(age < 40)
+            {
+                perfuct_ves = rost - 110;
+            }
+            else
+            {
+                perfuct_ves =  rost - 100;
+            }
+            return perfuct_ves;
+        }
+
+        public double Kuper_ves() //Идеальный вес по формуле Купера
+        {
+            double perfect_ves = 1;
+            if(pol == "Ж")
+            {
+                perfect_ves = (rost * 3.5 / 2.54 - 108) * 0.453;
+            }
+            else
+            {
+                perfect_ves = (rost * 4.0 / 2.54 - 128) * 0.453;
+            }
+            return perfect_ves;
+        }
+
+        public string imt()
+        {
+            string text;
+            double metr_rost = rost / 100;
+            double imt = ves / (Math.Pow(metr_rost, 2));
+            if (imt < 16) text = "Выраженный дифицит массы тела";
+            else if ((imt > 16) && (imt < 18.5)) text = "Недостаточная масса тела";
+            else if ((imt > 18.5) && (imt < 25)) text = "Норма";
+            else if ((imt > 25) && (imt < 30)) text = "Избыточная масса тела";
+            else if ((imt > 30) && (imt < 35)) text = "Ожирение 1 степени";
+            else if ((imt > 35) && (imt < 40)) text = "Ожирение 2 степени";
+            else text = "Ожирение 3 степени";
+            return text;
+        }
+    }
+}
